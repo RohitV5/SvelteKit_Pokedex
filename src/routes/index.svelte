98dfef8a1293a87d88c8dc2a42402fd76b3926ff
Rohit Verma <verma.rohit.in@gmail.com>
2021-12-31 14:54:14 +0530

    search functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,19 @@
 	import { pokemon } from "../stores/pokestore";
 	console.log($pokemon);
 	let searchTerm="";
-	let filteredPokeman
+	let filteredPokemon = [];
+
+
+	// any value inside this block changes then it executes
+	$:{
+
+		if(searchTerm){
+			filteredPokemon = $pokemon.filter(pokeman => pokeman.name.includes(searchTerm))
+		}else{
+			filteredPokemon = [...$pokemon];
+		}
+		console.log(filteredPokemon);
+	}
 </script>
 <svelte:head>
 	<title>Svelte Kit Pokedex</title>
@@ -11,7 +23,7 @@
 <h1 class="text-4xl text-center my-8 uppercase"> Svelte Kit Pokedex</h1>
 <input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} type="text" placeholder="Search Pokemon">
 <div class="py-4 grid gap-4 md:grid-cols-2 grids-col-1">
-{#each $pokemon as pokeman}
+{#each filteredPokemon as pokeman}
 	<PokemanCard pokeman={pokeman}></PokemanCard>
 {/each}
 </div>
