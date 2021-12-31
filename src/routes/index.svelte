fac158a6f67aef0adc6c16dd7633899caba40fc1
Rohit Verma <verma.rohit.in@gmail.com>
2021-12-31 14:23:59 +0530

    passing props to child component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,14 @@
 <script>
-	import Nav from "../components/nav.svelte";
+	import PokemanCard from "../components/pokemanCard.svelte";
 	import { pokemon } from "../stores/pokestore";
 	console.log($pokemon)
 </script>
 <svelte:head>
 	<title>Svelte Kit Pokedex</title>
 </svelte:head>
-<h1> Svelte Kit Pokedex</h1>
+<h1 class="text-4xl text-center my-8 uppercase"> Svelte Kit Pokedex</h1>
 
 {#each $pokemon as pokeman}
-<p class="text-red-500">{pokeman.name}</p>
+	<PokemanCard pokeman={pokeman}></PokemanCard>
 {/each}
 
