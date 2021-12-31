d7ab1b285d27620b710cf0e8a32b88669ddcaeb0
Rohit Verma <verma.rohit.in@gmail.com>
2021-12-31 15:39:59 +0530

    aling details page content

--- src/routes/pokemon/[id].svelte
+++ src/routes/pokemon/[id].svelte
@@ -15,10 +15,13 @@
     export let pokeman;
     const type = pokeman.types[0].type.name
 </script>
-<h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
-<p>Type: 
-    <strong>{type}</strong> | Height: <strong>{pokeman.height}</strong>
-    | Weight: <strong>{pokeman.weight} </strong>
+<div class="flex flex-col items-center">
+    <h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
+    <p>Type: 
+        <strong>{type}</strong> | Height: <strong>{pokeman.height}</strong>
+        | Weight: <strong>{pokeman.weight} </strong>
+    
+    </p>
+    <img class="card-image" src= {pokeman.sprites['front_default']} alt={pokeman.name}>
+</div>
 
-</p>
-<img class="card-image" src= {pokeman.sprites['front_default']} alt={pokeman.name}>
