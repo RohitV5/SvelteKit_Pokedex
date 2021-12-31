6d1d4d1288fb8db165f118b8e1516fca759f9317
Rohit Verma <verma.rohit.in@gmail.com>
2021-12-31 15:24:52 +0530

    dynamic params from url

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,33 @@
 <script>
-	import PokemanCard from "../components/pokemanCard.svelte";
-	import { pokemon } from "../stores/pokestore";
+	import PokemanCard from '../components/pokemanCard.svelte';
+	import { pokemon } from '../stores/pokestore';
 	console.log($pokemon);
-	let searchTerm="";
+	let searchTerm = '';
 	let filteredPokemon = [];
 
-
 	// any value inside this block changes then it executes
-	$:{
-
-		if(searchTerm){
-			filteredPokemon = $pokemon.filter(pokeman => pokeman.name.includes(searchTerm))
-		}else{
+	$: {
+		if (searchTerm) {
+			filteredPokemon = $pokemon.filter((pokeman) => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+		} else {
 			filteredPokemon = [...$pokemon];
 		}
 		console.log(filteredPokemon);
 	}
 </script>
+
 <svelte:head>
 	<title>Svelte Kit Pokedex</title>
 </svelte:head>
-<h1 class="text-4xl text-center my-8 uppercase"> Svelte Kit Pokedex</h1>
-<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} type="text" placeholder="Search Pokemon">
+<h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
+<input
+	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	bind:value={searchTerm}
+	type="text"
+	placeholder="Search Pokemon"
+/>
 <div class="py-4 grid gap-4 md:grid-cols-2 grids-col-1">
-{#each filteredPokemon as pokeman}
-	<PokemanCard pokeman={pokeman}></PokemanCard>
-{/each}
+	{#each filteredPokemon as pokeman}
+		<PokemanCard {pokeman} />
+	{/each}
 </div>
-
---
+++ src/routes/pokemon/[id].svelte
@@ -0,0 +1,17 @@
+<!-- server side javascript get the url param and fetch pokemon details and pass data through props-->
+<script context="module">
+    export async function load({params}){
+        const id = params.id;
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const res = await fetch(url);
+        const pokeman = await res.json();
+        return {props: {pokeman}}
+    }
+</script>
+
+<!-- client side javascript -->
+<script>
+    // expecting a prop by the name of pokeman which will be returned from serverside script
+    export let pokeman;
+</script>
+<h1>{pokeman.name}</h1>
