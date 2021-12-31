365d6e3983c68e26d14cdfb0dc3311d593ad308d
Rohit Verma <verma.rohit.in@gmail.com>
2021-12-31 13:06:11 +0530

    fetching data and adding to store

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Nav from "../components/nav.svelte";
-	console.log("hello world");
+	import { pokemon } from "../stores/pokestore";
+	console.log($pokemon)
 </script>
 <svelte:head>
 	<title>Svelte Kit Pokedex</title>
 </svelte:head>
 <h1> Svelte Kit Pokedex</h1>
+
